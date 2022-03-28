600254f38f6f71a23cf5e048ffddc6ccd4625c7f
ajidow <1368807530@qq.com>
2022-03-28 22:17:03 +0800

    task 2. And MSAA algorithm has bugs.

--- Assignment1/代码框架/main.cpp
+++ Assignment1/代码框架/main.cpp
@@ -57,15 +57,15 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
 Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
 {
 	angle = angle / 180.0f * MY_PI;
-	
+
 	Eigen::Matrix3f k;
-	k<<0.0f,-axis[2],axis[1],
-		axis[2],0.0f,-axis[0],
-		-axis[1],axis[0],0;
-	
+	k << 0.0f, -axis[2], axis[1],
+		axis[2], 0.0f, -axis[0],
+		-axis[1], axis[0], 0;
+
 	Eigen::Matrix3f E = Eigen::Matrix3f::Identity();
 	Eigen::Matrix3f rotation;
-	rotation << E*std::cos(angle) + (1-std::cos(angle))*axis*axis.transpose() +std::sin(angle)*k;
+	rotation << E * std::cos(angle) + (1 - std::cos(angle)) * axis * axis.transpose() + std::sin(angle) * k;
 	Eigen::Matrix4f model;
 	// for(int i = 0; i < 3; ++i)
 	// {
@@ -77,18 +77,18 @@ Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
 	// 	model(i,3)=0;
 	// }
 	// model(3,3)=1;
-	
-	model << rotation(0,0),rotation(0,1),rotation(0,2),0,
-			rotation(1.0),rotation(1,1),rotation(1,2),0,
-			rotation(2,0),rotation(2,1),rotation(2,2),0,
-			0,0,0,1;
+
+	model << rotation(0, 0), rotation(0, 1), rotation(0, 2), 0,
+		rotation(1.0), rotation(1, 1), rotation(1, 2), 0,
+		rotation(2, 0), rotation(2, 1), rotation(2, 2), 0,
+		0, 0, 0, 1;
 
 	return model;
 }
 
 int main(int argc, const char **argv)
 {
-	float anglex = 0,angley=0,anglez=0;
+	float anglex = 0, angley = 0, anglez = 0;
 	bool command_line = false;
 	std::string filename = "output.png";
 
@@ -139,12 +139,12 @@ int main(int argc, const char **argv)
 
 	char pre;
 
-	while (key != 27) //27 is esc...
+	while (key != 27) // 27 is esc...
 	{
 		r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-		//r.set_model(get_model_matrix(angle));
-		//r.set_model(get_rotation(axis, angle));
+		// r.set_model(get_model_matrix(angle));
+		// r.set_model(get_rotation(axis, angle));
 		r.set_view(get_view_matrix(eye_pos));
 		r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -157,7 +157,6 @@ int main(int argc, const char **argv)
 
 		std::cout << "frame count: " << frame_count++ << '\n';
 
-
 		if (key == 'x')
 		{
 			axis << 1.0f, 0.0f, 0.0f;
@@ -166,7 +165,7 @@ int main(int argc, const char **argv)
 		}
 		else if (key == 'y')
 		{
-			
+
 			axis << 0.0f, 1.0f, 0.0f;
 			angley += 10;
 			r.set_model(get_rotation(axis, angley));
@@ -177,7 +176,7 @@ int main(int argc, const char **argv)
 			anglez += 10;
 			r.set_model(get_rotation(axis, anglez));
 		}
-		
+
 		// if (angle >= 360.0f || angle <= -360.0f)
 		// 	angle = 0.0f;
 		pre = key;
---
+++ Hw2/code/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/development/Games101_Note/Games101_Note/Hw2/code/Triangle.cpp" "E:/development/Games101_Note/Games101_Note/Hw2/code/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.obj"
+  "E:/development/Games101_Note/Games101_Note/Hw2/code/main.cpp" "E:/development/Games101_Note/Games101_Note/Hw2/code/build/CMakeFiles/Rasterizer.dir/main.cpp.obj"
+  "E:/development/Games101_Note/Games101_Note/Hw2/code/rasterizer.cpp" "E:/development/Games101_Note/Games101_Note/Hw2/code/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
